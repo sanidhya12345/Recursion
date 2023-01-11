69a79c162d22a3c6b8621e0f711924f9bc66f56b
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-11 10:15:28 +0530

    print 1 to 5 using recursion

---
+++ Print1to5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Print1to5 {
+
+    private static void printItoV(int n){
+        if(n==5){
+            return ;
+        }
+        System.out.println(n+1);
+        printItoV(n+1);
+    } 
+    public static void main(String[] args) {
+        printItoV(0);    
+    }
+}
